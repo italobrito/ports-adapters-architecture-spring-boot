426b7ea8ffbd30182bfedc81417fa4da32cc34c4
Italo P. Brito <pbitalo@hotmail.com>
2023-12-21 09:56:02 -0300

    commit initial

---
+++ src/main/java/br/com/portsadapters/fastfoodapp/FastfoodappApplication.java
@@ -0,0 +1,13 @@
+package br.com.portsadapters.fastfoodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodappApplication.class, args);
+	}
+
+}
